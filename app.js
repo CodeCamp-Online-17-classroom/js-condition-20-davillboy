f9ba76c5b3bb735f81fffcf25fad4a91ffec784f
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 11:29:28 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+โค้ดด้านล่างทำงานได้หรือไม่
+
+let company = prompt('Which company created JavaScript?');
+company == 'Netscape' ? alert('Right!') : alert('Wrong.');
+
+ผลลัพธ์ 
+ทำงานได้ มีการใช้ Ternary Operators ในการตรวจสอบเงื่อนไข
+ถ้าผู้ใช้กรอกคำตอบเป็น Netscape จะแสดงข้อความ Right ถ้าไม่ใช่จะแสดงข้อความ Wrong
